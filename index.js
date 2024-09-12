86aac3698763d616113c04778293a1af7dcaa6d0
Kurniawan <kurniawanronaldi@gmail.com>
2024-09-12 16:02:03 +0700

    add metadata

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import fs from 'node:fs';
 import puppeteer from 'puppeteer';
 
-const username = '_yujin_an';
+const username = 'fallingin__fall';
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -19,14 +19,29 @@ const username = '_yujin_an';
     const images = Array.from(document.querySelectorAll('article img'));
     return images.map(image => image.src);
   });
-  images.map(async (image, index) => {
+  const metadata = await Promise.all(images.map(async (image, index) => {
     const page = await browser.newPage();
     const viewSource = await page.goto(image);
     const buffer = await viewSource.buffer();
+    const blob = new Blob([buffer], { type: viewSource.headers()['content-type'] });
     const ext = viewSource.headers()['content-type'].split('/')[1];
-    
     const filePath = `./images/${username}/image-${index + 1}.${ext}`;
     fs.mkdirSync(`./images/${username}`, { recursive: true, mode: 0o755, ifExists: true });
     fs.writeFileSync(filePath, buffer);
-  })
+
+    const id = new String(image.split('/')[5])
+    const pos = id.indexOf('?')
+
+    const meta = {
+      id: id.substring(0, pos),
+      url: image,
+      filename: `image-${index + 1}.${ext}`,
+      type: viewSource.headers()['content-type'],
+      ext: viewSource.headers()['content-type'].split('/')[1],
+      size: blob.size
+    }
+    return meta
+  }));
+  fs.writeFileSync(`./images/${username}/metadata.json`, JSON.stringify({ metadata }, null, 2));
+
 })();
